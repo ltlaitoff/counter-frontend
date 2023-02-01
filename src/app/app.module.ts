494233c834903f1def4147406bef98f3791f3416
ltlaitoff <ltlaitoff@gmail.com>
2023-02-01 19:26:35 +0200

    feat: Connected components and import HttpModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomeComponent } from './pages/home/home.component'
+import { CategoriesComponent } from './pages/categories/categories.component'
+import { StatisticComponent } from './pages/statistic/statistic.component'
+import { AuthorizationComponent } from './pages/authorization/authorization.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		CategoriesComponent,
+		StatisticComponent,
+		AuthorizationComponent
+	],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

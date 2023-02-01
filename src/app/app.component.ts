0b4f986beae1616182b20f64261cd9facf9284c7
ltlaitoff <ltlaitoff@gmail.com>
2023-02-01 19:26:35 +0200

    feat: Call authGuard.initialize on app component init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AuthGuardService } from './services/auth-guard.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'counter-frontend';
+export class AppComponent implements OnInit {
+	title = 'counter-frontend'
+
+	ngOnInit() {
+		this.authGuard.initialize()
+	}
+
+	constructor(private authGuard: AuthGuardService) {}
 }

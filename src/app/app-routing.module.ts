1eb49cc67218da617a4eb8132f096e4a0191c60d
ltlaitoff <ltlaitoff@gmail.com>
2023-02-01 19:26:35 +0200

    feat: Created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { CategoriesComponent } from './pages/categories/categories.component'
+import { StatisticComponent } from './pages/statistic/statistic.component'
+import { AuthorizationComponent } from './pages/authorization/authorization.component'
+import { AuthGuardService } from './services/auth-guard.service'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent,
+		canActivate: [AuthGuardService]
+	},
+	{
+		path: 'categories',
+		component: CategoriesComponent,
+		canActivate: [AuthGuardService]
+	},
+	{
+		path: 'statistic',
+		component: StatisticComponent,
+		canActivate: [AuthGuardService]
+	},
+	{
+		path: 'authorization',
+		component: AuthorizationComponent
+	},
+	{
+		path: '',
+		redirectTo: '/',
+		pathMatch: 'full'
+	}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

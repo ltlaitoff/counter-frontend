7d35800471a73f2de90457b9451e44ab4ca06646
ltlaitoff <ltlaitoff@gmail.com>
2023-02-02 18:25:28 +0200

    feat: Created Header input userInfo and template view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { User } from 'src/types/User'
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	@Input() userInfo: User | null = null
+}

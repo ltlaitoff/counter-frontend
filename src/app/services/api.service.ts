00c54e84cf28af1bdf45cb604422f00c4c3c4cf5
ltlaitoff <ltlaitoff@gmail.com>
2023-02-02 18:23:33 +0200

    feat: Using changed User type

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,10 +4,6 @@ import { User } from '../../types/User'
 
 const API_BASE_URL: string = 'http://localhost:8000'
 
-interface UserAuthorizationSucceed extends User {
-	authorized: true
-}
-
 @Injectable({
 	providedIn: 'root'
 })
@@ -17,28 +13,25 @@ export class ApiService {
 			authorized: false
 		}
 
-		const initialize = this.http.get<
-			InitializeFailed | UserAuthorizationSucceed
-		>(`${API_BASE_URL}/initialize`, {
-			withCredentials: true,
-			responseType: 'json'
-		})
+		const initialize = this.http.get<InitializeFailed | User>(
+			`${API_BASE_URL}/initialize`,
+			{
+				withCredentials: true,
+				responseType: 'json'
+			}
+		)
 
 		return initialize
 	}
 
 	authorization(JWTGoogleAuthorizationToken: string) {
-		return this.http.post<UserAuthorizationSucceed>(
-			`${API_BASE_URL}/authorization`,
-			null,
-			{
-				withCredentials: true,
-				responseType: 'json',
-				headers: new HttpHeaders({
-					Authorization: JWTGoogleAuthorizationToken
-				})
-			}
-		)
+		return this.http.post<User>(`${API_BASE_URL}/authorization`, null, {
+			withCredentials: true,
+			responseType: 'json',
+			headers: new HttpHeaders({
+				Authorization: JWTGoogleAuthorizationToken
+			})
+		})
 	}
 
 	constructor(private http: HttpClient) {}

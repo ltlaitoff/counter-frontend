3418ed334ae894e8669139eb0177dfea5fa95b33
ltlaitoff <ltlaitoff@gmail.com>
2023-02-02 17:02:54 +0200

    feat: Provider and module SocialAuth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 
+import {
+	SocialLoginModule,
+	SocialAuthServiceConfig,
+	GoogleLoginProvider
+} from '@abacritt/angularx-social-login'
+
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './pages/home/home.component'
@@ -9,6 +15,8 @@ import { CategoriesComponent } from './pages/categories/categories.component'
 import { StatisticComponent } from './pages/statistic/statistic.component'
 import { AuthorizationComponent } from './pages/authorization/authorization.component'
 import { HeaderComponent } from './components/header/header.component'
+
+import { environment } from 'src/environments/environment'
 
 @NgModule({
 	declarations: [
@@ -19,8 +27,31 @@ import { HeaderComponent } from './components/header/header.component'
 		AuthorizationComponent,
 		HeaderComponent
 	],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-	providers: [],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		SocialLoginModule
+	],
+	providers: [
+		{
+			provide: 'SocialAuthServiceConfig',
+			useValue: {
+				autoLogin: false,
+				providers: [
+					{
+						id: GoogleLoginProvider.PROVIDER_ID,
+						provider: new GoogleLoginProvider(environment.googleClientId, {
+							oneTapEnabled: false
+						})
+					}
+				],
+				onError: err => {
+					console.error(err)
+				}
+			} as SocialAuthServiceConfig
+		}
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}

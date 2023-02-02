f616b840f695d9c3952d79cbd431c8a6c3a1b29b
ltlaitoff <ltlaitoff@gmail.com>
2023-02-02 18:23:03 +0200

    feat: Changed User type: added authorized

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,12 @@
-export interface User {
+export interface UserBase {
 	_id: string
 	email: string
 	family_name: string
 	given_name: string
 	name: string
 	picture: string
+}
+
+export interface User extends UserBase {
+	authorized: boolean
 }

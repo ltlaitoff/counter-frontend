35517a9bd65667f18c0edd95e2e3413f80b27e85
ltlaitoff <ltlaitoff@gmail.com>
2023-02-05 18:30:14 +0200

    feat: Created basic statistic page

--- src/app/pages/statistic/statistic.component.ts
+++ src/app/pages/statistic/statistic.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Statistic } from 'src/types/Statistic'
+import { ApiService } from '../../services/api.service'
 
 @Component({
-  selector: 'app-statistic',
-  templateUrl: './statistic.component.html',
-  styleUrls: ['./statistic.component.scss']
+	selector: 'app-statistic',
+	templateUrl: './statistic.component.html',
+	styleUrls: ['./statistic.component.scss']
 })
-export class StatisticComponent {
+export class StatisticComponent implements OnInit {
+	statistics: Statistic[] | null = null
 
+	ngOnInit() {
+		this.api.getAllStatisticRecords().subscribe(statistics => {
+			this.statistics = statistics
+		})
+	}
+
+	constructor(private api: ApiService) {}
 }

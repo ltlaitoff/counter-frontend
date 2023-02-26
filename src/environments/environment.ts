b768eed3f5f831959f9f51b08f1ecf81f821a3fa
ltlaitoff <ltlaitoff@gmail.com>
2023-02-27 01:59:53 +0200

    feat: Update production google client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
-	googleClientId: 'PROD_GOOGLE_CLIEND_ID'
+	googleClientId:
+		'446806438760-0h3fblc15rjdl05rqtui4ki0lkpqlr08.apps.googleusercontent.com'
 }

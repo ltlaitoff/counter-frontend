5e51e912f237970262f99a99e11d98e7d1db5aeb
ltlaitoff <ltlaitoff@gmail.com>
2023-02-28 03:47:44 +0200

    feat: Created ApiEffects

---
+++ src/app/store/effects/api.effects.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { select, Store } from '@ngrx/store'
+import { EMPTY } from 'rxjs'
+import { map, exhaustMap, catchError, withLatestFrom } from 'rxjs/operators'
+import { ApiService } from 'src/app/services/api.service'
+
+import { ColorsActions } from '../colors'
+import { RootState } from '../rootTypes'
+
+@Injectable()
+export class ApiEffects {
+	loadColors$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(ColorsActions.loadColors),
+			withLatestFrom(this.store.pipe(select('colors'))),
+			exhaustMap(([params, colorsValue]) => {
+				console.log(params, colorsValue)
+				if (colorsValue.length === 0 || params.force) {
+					return this.api.getAllColors().pipe(
+						map(colors =>
+							ColorsActions.colorsLoadedSuccess({ payload: colors })
+						),
+						catchError(() => EMPTY)
+					)
+				}
+
+				return EMPTY
+			})
+		)
+	)
+
+	constructor(
+		private actions$: Actions,
+		private api: ApiService,
+		private store: Store<RootState>
+	) {}
+}

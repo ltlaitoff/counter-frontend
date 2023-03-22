ebc766f65af6bf76015de2556292a4f1456741d7
ltlaitoff <ltlaitoff@gmail.com>
2023-03-22 19:17:03 +0200

    fix: Removed / from path in env API_HOST

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 	production: true,
 	googleClientId:
 		'446806438760-0h3fblc15rjdl05rqtui4ki0lkpqlr08.apps.googleusercontent.com',
-	API_HOST: 'https://counter-api-git-dev-ltlaitoff.vercel.app/'
+	API_HOST: 'https://counter-api-git-dev-ltlaitoff.vercel.app'
 }

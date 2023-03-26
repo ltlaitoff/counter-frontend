c51e49a1526f0429e126e0013fdd9d35eb5235e6
ltlaitoff <ltlaitoff@gmail.com>
2023-03-26 15:31:13 +0300

    feat: Created user-panel

--- src/app/components/header/user-info/user-info.component.ts
+++ src/app/components/header/user-info/user-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { User } from 'src/types/User'
+import { AuthGuardService } from '../../../services/auth-guard.service'
 
 @Component({
 	selector: 'app-user-info',
@@ -7,5 +8,33 @@ import { User } from 'src/types/User'
 	styleUrls: ['./user-info.component.scss']
 })
 export class UserInfoComponent {
+	constructor(private authGuard: AuthGuardService) {}
+
 	@Input() userInfo: User | null = null
+
+	isOpened = false
+
+	clickedOutside() {
+		if (this.userInfo === null) return
+		if (this.isOpened === false) return
+
+		this.isOpened = false
+	}
+
+	private toggleDropdownOpened() {
+		this.isOpened = !this.isOpened
+	}
+
+	private closeDropdown() {
+		this.isOpened = false
+	}
+
+	onBadgeClick() {
+		this.toggleDropdownOpened()
+	}
+
+	onExitClick() {
+		this.authGuard.unauthorize()
+		this.closeDropdown()
+	}
 }

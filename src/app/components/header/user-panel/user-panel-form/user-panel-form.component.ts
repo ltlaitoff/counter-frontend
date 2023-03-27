d7461e2bbce7e7140806bb373a37409b67b41250
ltlaitoff <ltlaitoff@gmail.com>
2023-03-27 07:48:11 +0300

    feat: Moved user-panel functional into subcomponents

--- src/app/components/header/user-panel/user-panel-form/user-panel-form.component.ts
+++ src/app/components/header/user-panel/user-panel-form/user-panel-form.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 @Component({
 	selector: 'app-user-panel-form',
 	templateUrl: './user-panel-form.component.html'
 })
-export class UserPanelFormComponent {}
+export class UserPanelFormComponent {
+	@Output() exitClick = new EventEmitter()
+	@Input() isOpened: boolean = false
+
+	onExitClick() {
+		this.exitClick.emit()
+	}
+}
--- src/app/components/header/user-panel/user-panel-open-button/user-panel-open-button.component.ts
+++ src/app/components/header/user-panel/user-panel-open-button/user-panel-open-button.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { User } from 'src/types/User'
 
 @Component({
 	selector: 'app-user-panel-open-button',
 	templateUrl: './user-panel-open-button.component.html'
 })
-export class UserPanelOpenButtonComponent {}
+export class UserPanelOpenButtonComponent {
+	@Input() userInfo: User | null = null
+	@Input() isOpened: boolean = false
+
+	@Output() buttonClick = new EventEmitter()
+
+	onButtonClick() {
+		this.buttonClick.emit()
+	}
+}

49572faa604dcf373788a0611fa9aad7f817a219
ltlaitoff <ltlaitoff@gmail.com>
2023-03-27 10:59:51 +0300

    feat: Renamed home.component to home-page.component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { HomeComponent } from './home.component'
+import { HomeComponent } from './home-page/home-page.component'
 import { CategoriesModule } from '../categories/categories.module'
 import { StatisticModule } from '../statistic/statistic.module'
 import { ReactiveFormsModule } from '@angular/forms'

338635e8495238fa351d19c06fae67390467a0f7
ltlaitoff <ltlaitoff@gmail.com>
2023-03-27 07:49:43 +0300

    feat: Removed isOpened prop in user-panel-form

--- src/app/components/header/user-panel/user-panel-form/user-panel-form.component.ts
+++ src/app/components/header/user-panel/user-panel-form/user-panel-form.component.ts
@@ -6,7 +6,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
 })
 export class UserPanelFormComponent {
 	@Output() exitClick = new EventEmitter()
-	@Input() isOpened: boolean = false
 
 	onExitClick() {
 		this.exitClick.emit()

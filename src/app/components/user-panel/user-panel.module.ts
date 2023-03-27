5444246aa20fe616754b64fff4a2251da5ff2c5f
ltlaitoff <ltlaitoff@gmail.com>
2023-03-27 08:27:10 +0300

    feat: Created user-panel-form-item

--- src/app/components/user-panel/user-panel.module.ts
+++ src/app/components/user-panel/user-panel.module.ts
@@ -4,13 +4,15 @@ import { UserPanelComponent } from './user-panel.component'
 import { UserPanelPlugComponent } from './user-panel-plug/user-panel-plug.component'
 import { UserPanelOpenButtonComponent } from './user-panel-open-button/user-panel-open-button.component'
 import { UserPanelFormComponent } from './user-panel-form/user-panel-form.component'
+import { UserPanelFormItemComponent } from './user-panel-form-item/user-panel-form-item.component'
 
 @NgModule({
 	declarations: [
 		UserPanelComponent,
 		UserPanelPlugComponent,
 		UserPanelOpenButtonComponent,
-		UserPanelFormComponent
+		UserPanelFormComponent,
+		UserPanelFormItemComponent
 	],
 	imports: [CommonModule],
 	exports: [UserPanelComponent]

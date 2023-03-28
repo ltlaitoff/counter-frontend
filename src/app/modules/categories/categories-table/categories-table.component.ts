f2544091f9a9acbda8d75627858fec51a2e535ae
ltlaitoff <ltlaitoff@gmail.com>
2023-03-28 18:16:51 +0300

    feat(categories): Created synchronization icon error

--- src/app/modules/categories/categories-table/categories-table.component.ts
+++ src/app/modules/categories/categories-table/categories-table.component.ts
@@ -3,7 +3,8 @@ import { Store } from '@ngrx/store'
 import { sortedByOrder } from 'src/app/helpers'
 import { RootState } from 'src/app/store'
 import { selectCategories, CategoriesActions } from 'src/app/store/categories'
-import { Category } from 'src/types/Category'
+import { CategoryStateItem } from 'src/app/store/categories/categories.types'
+import { Status } from 'src/app/store/categories/not-sync/categories-not-sync.types'
 
 @Component({
 	selector: 'counter-categories-table',
@@ -11,7 +12,7 @@ import { Category } from 'src/types/Category'
 	styleUrls: ['./categories-table.component.scss']
 })
 export class CategoriesTableComponent implements OnInit {
-	categories: Category[] | null = null
+	categories: CategoryStateItem[] | null = null
 
 	ngOnInit() {
 		this.store.select(selectCategories).subscribe(value => {
@@ -23,8 +24,26 @@ export class CategoriesTableComponent implements OnInit {
 		return sortedByOrder(this.categories)
 	}
 
-	deleteCategory(category: Category) {
+	deleteCategory(category: CategoryStateItem) {
 		this.store.dispatch(CategoriesActions.delete(category))
+	}
+
+	checkStatusIs(
+		status: Status | undefined,
+		value: 'not-synchronized' | 'synchronization' | 'error'
+	) {
+		if (!status) return
+
+		switch (value) {
+			case 'not-synchronized':
+				return status === Status.NOT_SYNCHRONIZED
+
+			case 'synchronization':
+				return status === Status.SYNCHRONIZATION
+
+			case 'error':
+				return status === Status.ERROR
+		}
 	}
 
 	constructor(private store: Store<RootState>) {}

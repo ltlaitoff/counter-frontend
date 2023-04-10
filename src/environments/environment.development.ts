8cffd8a00836fd439776fc9e6d8394b03d0a67d2
ltlaitoff <ltlaitoff@gmail.com>
2023-04-10 18:58:57 +0300

    chore: Removed production and added type fields to environment
    * Production field removed due to uselessness
    * Type field added. Type fields is configuration type from angular.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-	version: '1.0.0 dev',
-	production: false,
+	version: '1.0.0',
+	type: 'dev',
 	googleClientId:
 		'446806438760-0h3fblc15rjdl05rqtui4ki0lkpqlr08.apps.googleusercontent.com',
 	API_HOST: 'https://localhost:8000'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	version: '1.0.2',
-	production: true,
+	type: 'production',
 	googleClientId:
 		'446806438760-0h3fblc15rjdl05rqtui4ki0lkpqlr08.apps.googleusercontent.com',
 	API_HOST: 'https://counter-api.vercel.app'

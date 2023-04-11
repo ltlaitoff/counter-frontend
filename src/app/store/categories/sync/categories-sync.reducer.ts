bb4e099638b1b6d2fc9e766bcbd3a1cb82a24595
ltlaitoff <ltlaitoff@gmail.com>
2023-04-11 21:25:30 +0300

    feat(store/categories): Created reducer for orderUpdate categories/sync action

--- src/app/store/categories/sync/categories-sync.reducer.ts
+++ src/app/store/categories/sync/categories-sync.reducer.ts
@@ -10,5 +10,19 @@ export const categoriesReducer = createReducer(
 	on(CategoriesSyncActions.add, (state, { category }) => [...state, category]),
 	on(CategoriesSyncActions.delete, (state, category) => {
 		return [...state.filter(item => item._id !== category._id)]
+	}),
+	on(CategoriesSyncActions.orderupdate, (state, { data }) => {
+		return [
+			...state.map(item => {
+				if (item._id === data.categoryId) {
+					return {
+						...item,
+						order: data.currentIndex
+					}
+				}
+
+				return item
+			})
+		]
 	})
 )

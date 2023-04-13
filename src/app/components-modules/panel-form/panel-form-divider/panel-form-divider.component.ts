7727677089d56645590a2bdc0eb5fc8aba9dd366
ltlaitoff <ltlaitoff@gmail.com>
2023-04-13 21:11:34 +0300

    feat(panel-form): Created panel-form in components-modules

---
+++ src/app/components-modules/panel-form/panel-form-divider/panel-form-divider.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'counter-panel-form-divider',
+	templateUrl: './panel-form-divider.component.html'
+})
+export class PanelFormDividerComponent {}

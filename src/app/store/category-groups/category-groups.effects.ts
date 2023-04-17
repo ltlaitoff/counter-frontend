a8611949b303496a50276831172a762a3ca7a7e0
ltlaitoff <ltlaitoff@gmail.com>
2023-04-17 17:21:36 +0300

    feat(store/category-groups): Created category-groups.effects

---
+++ src/app/store/category-groups/category-groups.effects.ts
@@ -0,0 +1,307 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { select, Store } from '@ngrx/store'
+import { EMPTY, of } from 'rxjs'
+import { switchMap, catchError, withLatestFrom, mergeMap } from 'rxjs/operators'
+import { ApiService } from 'src/app/services/api.service'
+import { CategoryGroupsActions } from './category-groups.actions'
+import { CategoryGroupsNotSyncActions } from './not-sync/category-groups-not-sync.actions'
+import { CategoryGroupsSyncActions } from './sync/category-groups-sync.actions'
+
+import { RootState } from '../rootTypes'
+import { NotSyncHelpers } from './not-sync'
+import { CategoryGroupsNotSyncStateItem } from './category-groups.types'
+import { NotSyncStatus } from '../store.types'
+
+@Injectable()
+export class CategoryGroupsEffects {
+	/* Redirects */
+	add$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.add),
+			mergeMap(payloadf => {
+				const payloadAsNotSyncStateItem: CategoryGroupsNotSyncStateItem =
+					NotSyncHelpers.changeAddCategoryGroupValueToStoreItem(payloadf)
+
+				return of(
+					CategoryGroupsNotSyncActions.add(payloadAsNotSyncStateItem),
+					CategoryGroupsActions.addeffect(payloadAsNotSyncStateItem)
+				)
+			})
+		)
+	)
+
+	update$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.update),
+			mergeMap(payload => {
+				if (payload.old.status) {
+					return [
+						CategoryGroupsNotSyncActions.update({
+							oldCategory: payload.old,
+							dataForUpdate: payload.dataForUpdate
+						})
+					]
+				}
+
+				const payloadAsNotSyncStateItem: CategoryGroupsNotSyncStateItem =
+					NotSyncHelpers.changeUpdateCategoryGroupValueToStoreItem(
+						payload.old,
+						payload.dataForUpdate
+					)
+
+				return of(
+					CategoryGroupsNotSyncActions.add(payloadAsNotSyncStateItem),
+					CategoryGroupsSyncActions.delete(payload.old),
+					CategoryGroupsActions.updateeffect(payloadAsNotSyncStateItem)
+				)
+			})
+		)
+	)
+
+	// reorder$ = createEffect(() =>
+	// 	this.actions$.pipe(
+	// 		ofType(CategoryGroupsActions.reorder),
+	// 		switchMap(({ props, previousIndex, currentIndex }) => {
+	// 			const categoryAsStateItem = props
+
+	// 			if (categoryAsStateItem.status) {
+	// 				return of()
+	// 			}
+
+	// 			const oldCategoryAsCategoryGroupsNotSyncStateItem: CategoryGroupsNotSyncStateItem =
+	// 				{
+	// 					...NotSyncHelpers.changeUpdateCategoryGroupValueToStoreItem(
+	// 						categoryAsStateItem,
+	// 						{}
+	// 					),
+	// 					order:
+	// 						previousIndex < currentIndex ? currentIndex + 1 : currentIndex
+	// 				}
+
+	// 			return of(
+	// 				CategoryGroupsNotSyncActions.add(
+	// 					oldCategoryAsCategoryGroupsNotSyncStateItem
+	// 				),
+	// 				CategoryGroupsSyncActions.delete(categoryAsStateItem),
+	// 				CategoryGroupsActions.reordereffect({
+	// 					props: oldCategoryAsCategoryGroupsNotSyncStateItem,
+	// 					previousIndex,
+	// 					currentIndex
+	// 				})
+	// 			)
+	// 		})
+	// 	)
+	// )
+
+	delete$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.delete),
+			switchMap(payload => {
+				if (payload.status) {
+					return of(CategoryGroupsNotSyncActions.delete(payload))
+				}
+
+				const payloadAsNotSyncStateItem =
+					NotSyncHelpers.changeDeleteCategoryGroupValueToStoreItem(payload)
+
+				return of(
+					CategoryGroupsNotSyncActions.add(payloadAsNotSyncStateItem),
+					CategoryGroupsSyncActions.delete(payload),
+					CategoryGroupsActions.deleteeffect(payloadAsNotSyncStateItem)
+				)
+			})
+		)
+	)
+	/* Redirects end */
+
+	/* Main effects */
+	loadCategoryGroups$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.load),
+			// TODO: Change categories after set in root
+			withLatestFrom(this.store.pipe(select('categories'))),
+			switchMap(([params, categoriesValue]) => {
+				if (categoriesValue.length === 0 || params.force) {
+					return this.api.getAllCategoryGroups().pipe(
+						mergeMap(value =>
+							of(CategoryGroupsSyncActions.set({ payload: value }))
+						),
+						catchError(() => EMPTY)
+					)
+				}
+
+				return EMPTY
+			})
+		)
+	)
+
+	addCategory$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.addeffect),
+			switchMap(payload => {
+				this.store.dispatch(
+					CategoryGroupsNotSyncActions.changestatus({
+						status: NotSyncStatus.SYNCHRONIZATION,
+						payload: payload
+					})
+				)
+
+				return this.api.addCategoryGroups(payload).pipe(
+					switchMap(resultCategory =>
+						of(
+							CategoryGroupsSyncActions.add({
+								payload: resultCategory
+							}),
+							CategoryGroupsNotSyncActions.delete(payload)
+						)
+					),
+					catchError(() =>
+						of(
+							CategoryGroupsNotSyncActions.changestatus({
+								status: NotSyncStatus.ERROR,
+								payload: payload
+							})
+						)
+					)
+				)
+			})
+		)
+	)
+
+	deleteCategory$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.deleteeffect),
+			switchMap(payload => {
+				this.store.dispatch(
+					CategoryGroupsNotSyncActions.changestatus({
+						status: NotSyncStatus.SYNCHRONIZATION,
+						payload: payload
+					})
+				)
+
+				return this.api.deleteCategoryGroups(payload._id).pipe(
+					switchMap(() => of(CategoryGroupsNotSyncActions.delete(payload))),
+					catchError(() =>
+						of(
+							CategoryGroupsNotSyncActions.changestatus({
+								status: NotSyncStatus.ERROR,
+								payload: payload
+							})
+						)
+					)
+				)
+			})
+		)
+	)
+
+	updateCategory$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(CategoryGroupsActions.updateeffect),
+			switchMap(payload => {
+				this.store.dispatch(
+					CategoryGroupsNotSyncActions.changestatus({
+						status: NotSyncStatus.SYNCHRONIZATION,
+						payload: payload
+					})
+				)
+
+				return this.api.updateCategoryGroups(payload._id, payload).pipe(
+					switchMap(resultCategory =>
+						of(
+							CategoryGroupsSyncActions.add({
+								payload: resultCategory
+							}),
+
+							CategoryGroupsNotSyncActions.delete(payload)
+						)
+					),
+					catchError(() => {
+						return of(
+							CategoryGroupsNotSyncActions.changestatus({
+								status: NotSyncStatus.ERROR,
+								payload: payload
+							})
+						)
+					})
+				)
+			})
+		)
+	)
+
+	// reorderCategory$ = createEffect(() =>
+	// 	this.actions$.pipe(
+	// 		ofType(CategoryGroupsActions.reordereffect),
+
+	// 		switchMap(({ category, previousIndex, currentIndex }) => {
+	// 			this.store.dispatch(
+	// 				CategoryGroupsNotSyncActions.changestatus({
+	// 					status: NotSyncStatus.SYNCHRONIZATION,
+	// 					category: category
+	// 				})
+	// 			)
+
+	// 			console.log('data for reorder:', {
+	// 				categoryId: category._id,
+	// 				previousIndex,
+	// 				currentIndex
+	// 			})
+
+	// 			return this.api
+	// 				.reorderCategory({
+	// 					categoryId: category._id,
+	// 					previousIndex,
+	// 					currentIndex
+	// 				})
+	// 				.pipe(
+	// 					switchMap(resultValue =>
+	// 						of(
+	// 							...resultValue.map(value => {
+	// 								if (value.categoryId === category._id) {
+	// 									return CategoryGroupsSyncActions.add({
+	// 										category: {
+	// 											...category,
+	// 											order: value.currentIndex,
+	// 											status: undefined,
+	// 											action: undefined
+	// 										}
+	// 									})
+	// 								}
+
+	// 								return CategoryGroupsSyncActions.orderupdate({ data: value })
+	// 							}),
+	// 							CategoryGroupsStatusActions.set({
+	// 								status: CategoryGroupsStatusTypes.StatusState.SYNCHRONIZED
+	// 							}),
+	// 							CategoryGroupsNotSyncActions.delete(category)
+	// 						)
+	// 					),
+	// 					catchError(() => {
+	// 						this.store.dispatch(
+	// 							CategoryGroupsNotSyncActions.changestatus({
+	// 								status: NotSyncStatus.ERROR,
+	// 								category: category
+	// 							})
+	// 						)
+
+	// 						this.store.dispatch(
+	// 							CategoryGroupsStatusActions.set({
+	// 								status: CategoryGroupsStatusTypes.StatusState.ERROR
+	// 							})
+	// 						)
+
+	// 						return EMPTY
+	// 					})
+	// 				)
+	// 		})
+	// 	)
+	// )
+
+	/* Main effects end */
+
+	constructor(
+		private actions$: Actions,
+		private api: ApiService,
+		private store: Store<RootState>
+	) {}
+}

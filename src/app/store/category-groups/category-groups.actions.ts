87e92cccd39721457d51788d99e4797848e059d8
ltlaitoff <ltlaitoff@gmail.com>
2023-04-17 15:58:00 +0300

    feat(store/category-groups): Created category-groups.actions

---
+++ src/app/store/category-groups/category-groups.actions.ts
@@ -0,0 +1,37 @@
+import { createActionGroup, props } from '@ngrx/store'
+
+import { AddCategoryGroupInputs } from 'src/types/ApiInputs'
+import {
+	CategoryGroupsNotSyncStateItem,
+	CategoryGroupsStateItem,
+	CategoryGroupsSyncStateItem
+} from './category-groups.types'
+
+export const CategoryGroupsActions = createActionGroup({
+	source: 'CategoryGroups',
+	events: {
+		load: (props: { force: boolean } = { force: false }) => props,
+
+		add: (props: AddCategoryGroupInputs) => props,
+		delete: (props: CategoryGroupsStateItem) => props,
+		update: props<{
+			oldCategory: CategoryGroupsStateItem
+
+			dataForUpdate: AddCategoryGroupInputs
+		}>(),
+		reorder: props<{
+			props: CategoryGroupsSyncStateItem
+			previousIndex: number
+			currentIndex: number
+		}>(),
+
+		addEffect: (props: CategoryGroupsNotSyncStateItem) => props,
+		deleteEffect: (props: CategoryGroupsNotSyncStateItem) => props,
+		updateEffect: (props: CategoryGroupsNotSyncStateItem) => props,
+		reorderEffect: props<{
+			props: CategoryGroupsNotSyncStateItem
+			previousIndex: number
+			currentIndex: number
+		}>()
+	}
+})

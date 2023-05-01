c3b13332630d18d936a70c6eb26fd0c51933b054
ltlaitoff <ltlaitoff@gmail.com>
2023-05-01 20:28:39 +0300

    feat(store/category-groups): Created  in category-groups.types

--- src/app/store/category-groups/category-groups.types.ts
+++ src/app/store/category-groups/category-groups.types.ts
@@ -4,14 +4,29 @@ import {
 } from 'src/app/store/store.types'
 
 import { CategoryGroup } from 'src/types/CategoryGroup'
+import { Color } from 'src/types/Color'
+
+export type CategoryGroupsItemToWithColor<T extends { color: string }> = Omit<
+	T,
+	'color'
+> & {
+	color: Color
+}
 
 export type CategoryGroupsNotSyncStateItem = NotSyncStateItemBase &
 	CategoryGroup
 export type CategoryGroupsNotSyncState = CategoryGroupsNotSyncStateItem[]
+export type CategoryGroupsNotSyncStateItemWithColor =
+	CategoryGroupsItemToWithColor<CategoryGroupsNotSyncStateItem>
 
 export type CategoryGroupsSyncStateItem = SyncStateItemBase & CategoryGroup
 export type CategoryGroupsSyncState = CategoryGroupsSyncStateItem[]
+export type CategoryGroupsSyncStateItemWithColor =
+	CategoryGroupsItemToWithColor<CategoryGroupsSyncStateItem>
 
 export type CategoryGroupsStateItem =
 	| CategoryGroupsSyncStateItem
 	| CategoryGroupsNotSyncStateItem
+export type CategoryGroupsStateItemWithColor =
+	| CategoryGroupsNotSyncStateItemWithColor
+	| CategoryGroupsSyncStateItemWithColor

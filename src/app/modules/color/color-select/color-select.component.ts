83cf642d54ef492c1b94e5d0c98b72fdfb996c42
ltlaitoff <ltlaitoff@gmail.com>
2023-05-07 11:04:13 +0300

    feat(color): Created `Color` module with `color-select` component

---
+++ src/app/modules/color/color-select/color-select.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core'
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
+import { Store } from '@ngrx/store'
+import { sortedByOrder } from 'src/app/helpers'
+import { RootState } from 'src/app/store'
+import { selectColors } from 'src/app/store/colors'
+import { Color } from 'src/types/Color'
+
+@Component({
+	selector: 'counter-color-select',
+	templateUrl: './color-select.component.html',
+	styleUrls: ['color-select.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			multi: true,
+			useExisting: ColorSelectComponent
+		}
+	]
+})
+export class ColorSelectComponent implements ControlValueAccessor {
+	colors: Color[] | null = null
+	choicedColorId: string | null = null
+
+	ngOnInit() {
+		this.store.select(selectColors).subscribe(newColors => {
+			this.colors = newColors
+
+			if (this.choicedColorId === null) {
+				this.choicedColorId = newColors[0]?._id || null
+			}
+		})
+	}
+
+	get sortedByOrderColors() {
+		return sortedByOrder(this.colors)
+	}
+
+	checkColor(colorId: string) {
+		this.markAsTouched()
+
+		if (this.disabled) return
+
+		this.choicedColorId = colorId
+		this.onChange(colorId)
+	}
+
+	onChange: any = (colorId: string) => {}
+	onTouched: any = () => {}
+
+	disabled: boolean = false
+	touched: boolean = false
+
+	writeValue(colorId: string | null): void {
+		if (colorId === null) return
+
+		this.choicedColorId = colorId
+	}
+
+	registerOnChange(callback: (_: any) => void): void {
+		this.onChange = callback
+	}
+
+	registerOnTouched(callback: (_: any) => void): void {
+		this.onTouched = callback
+	}
+
+	setDisabledState(disabled: boolean) {
+		this.disabled = disabled
+	}
+
+	markAsTouched() {
+		if (!this.touched) {
+			this.onTouched()
+			this.touched = true
+		}
+	}
+
+	constructor(private store: Store<RootState>) {}
+}
